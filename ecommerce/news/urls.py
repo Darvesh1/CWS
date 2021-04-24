d10627c08f162aa9de2854644b7b6df392983e5d
Darvesh1 <aminjony@gmail.com>
2021-04-24 13:22:41 +0600

    Fin Lesson 21

--- ecommerce/news/urls.py
+++ ecommerce/news/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-# from . import views
 from .views import *
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
     path('', index, name="home"),
     path('news/category/<int:category_id>', get_category, name='category'),
+    path('news/<int:news_id>', view_news, name='view_news'),
 ]
